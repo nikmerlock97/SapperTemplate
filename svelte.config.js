67f351dceeba2de54539594af0d58507aec35ebb
Nik Merlock <39422774+nikmerlock97@users.noreply.github.com>
2020-12-04 19:14:16 -0600

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+const sveltePreprocess = require("svelte-preprocess");
+const postcss = require("./postcss.config");
+
+const createPreprocessors = ({ sourceMap }) => [
+	sveltePreprocess({
+		sourceMap,
+		defaults: {
+			script: "typescript",
+			style: "postcss",
+		},
+		scss: {
+      includePaths: ['./src/theme'],
+    },
+		postcss,
+	}),
+	// You could have more preprocessors, like mdsvex
+];
+
+module.exports = {
+	createPreprocessors,
+	// Options for `svelte-check` and the VS Code extension
+	preprocess: createPreprocessors({ sourceMap: true }),
+};
